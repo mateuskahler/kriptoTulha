611dd31abed073cba128c8f7e2d1eb898a6226d3
Mateus Kahler <mateus.kahler@gmail.com>
2022-07-17 20:47:04 -0300

    enforce line size

--- crypto/stamp.py
+++ crypto/stamp.py
@@ -9,7 +9,8 @@ def generate_integrity_stamp(message: memoryview, crypo_key: bytes) -> bytes:
     return bytes(calculated_hash + salt)
 
 
-def validate_integrity_stamp(message: memoryview, stamp: bytes, crypo_key: bytes) -> bool:
+def validate_integrity_stamp(message: memoryview, stamp: bytes,
+                             crypo_key: bytes) -> bool:
     from .parameters import INTEGRITY_STAMP_HASH_SIZE, \
         INTEGRITY_STAMP_SALT_SIZE
 
@@ -17,7 +18,9 @@ def validate_integrity_stamp(message: memoryview, stamp: bytes, crypo_key: bytes
     provided_stamp_size = len(stamp)
     if provided_stamp_size != expected_stamp_size:
         raise RuntimeError(
-            f'Integrity stamp has wrong size. Expected {expected_stamp_size}, received {provided_stamp_size}.')
+            f'Integrity stamp has wrong size. '
+            f'Expected {expected_stamp_size}, '
+            f'received {provided_stamp_size}.')
 
     provided_hash = stamp[:INTEGRITY_STAMP_HASH_SIZE]
     salt = stamp[INTEGRITY_STAMP_HASH_SIZE:]
@@ -26,13 +29,17 @@ def validate_integrity_stamp(message: memoryview, stamp: bytes, crypo_key: bytes
     return compare_digest(calculated_hash, provided_hash)
 
 
-def generate_hash(message: memoryview, crypo_key: bytes, salt: bytes = None) -> tuple[bytes, bytes]:
+def generate_hash(message: memoryview, crypo_key: bytes,
+                  salt: bytes = None) -> tuple[bytes, bytes]:
     from .parameters import INTEGRITY_STAMP_HASH_SIZE, \
         INTEGRITY_STAMP_SALT_SIZE
 
     salt = validate_salt(salt, INTEGRITY_STAMP_SALT_SIZE)
 
     hasher = blake2b(
-        message, digest_size=INTEGRITY_STAMP_HASH_SIZE, key=crypo_key, salt=salt)
+        message,
+        digest_size=INTEGRITY_STAMP_HASH_SIZE,
+        key=crypo_key,
+        salt=salt)
 
     return hasher.digest(), salt
