0aeb22c01a6a9d39a19b23c2d98a4ce7dc53fcdb
Mateus Kahler <mateus.kahler@gmail.com>
2022-09-05 22:00:12 -0300

    basic load file flow

--- user_interface/components/itens_navigation.py
+++ user_interface/components/itens_navigation.py
@@ -1,18 +1,28 @@
 import tkinter as tk
 from tkinter import Frame, StringVar, ttk, font
+from typing import Callable
+
+from tulha import ItemsCompilation
 
 
 class ItemsNavigator:
-    def __init__(self, parent: Frame):
+    def __init__(self, parent: Frame,
+                 get_content_callback: Callable[[], ItemsCompilation],
+                 item_selected_callback: Callable[[int | None], None]):
         frame = ttk.Frame(parent, padding="0 5 5 5")
         frame.grid(column=0, row=0, sticky="nsew")
 
         self.serch_bar = ItemsSearchBar(frame)
-        self.titles_list = ItemsTitleList(frame)
+        self.titles_list = ItemsTitleList(
+            frame, get_content_callback, item_selected_callback)
 
         frame.columnconfigure(0, weight=1)
         frame.rowconfigure(0, weight=0)
         frame.rowconfigure(1, weight=1)
+
+    def reset_visibility(self):
+        self.serch_bar.clear_search()
+        self.titles_list.update_visible_list()
 
 
 class ItemsSearchBar:
@@ -36,17 +46,25 @@ class ItemsSearchBar:
         frame.columnconfigure(1, weight=1)
         frame.rowconfigure(0, weight=1)
 
+    def clear_search(self):
+        self.search_field.delete(0, 'end')
+
 
 class ItemsTitleList:
-    def __init__(self, parent: Frame):
-        self.callback_de_item_selecionado = None
+    def __init__(self, parent: Frame,
+                 get_content_callback: Callable[[], ItemsCompilation],
+                 item_selected_callback: Callable[[int | None], None]):
+        self.get_content_callback = get_content_callback
+        self.item_selected_callback = item_selected_callback
+
+        self.iid_last_selected_item: int | None = None
 
         frame = ttk.Frame(parent, padding="0 5 0 0")
         frame.grid(column=0, row=1, sticky="nsew")
 
         titles_list = ttk.Treeview(frame, selectmode="browse")
-        titles_list['columns'] = ('título')
-        titles_list.heading(0, text='Item')
+        titles_list['columns'] = ('titles')
+        titles_list.heading(0, text='Items')
         titles_list_scroll = tk.Scrollbar(frame, orient=tk.VERTICAL,
                                           command=titles_list.yview)
         titles_list_scroll.grid(column=1, row=0, sticky='ns')
@@ -54,11 +72,40 @@ class ItemsTitleList:
         titles_list.tag_configure('modificado', font=font.Font(slant='italic'))
 
         titles_list.bind('<<TreeviewSelect>>',
-                         self.callback_de_item_selecionado)
+                         self.item_selected)
         titles_list.column('#0', width=0, stretch=tk.NO)
 
         titles_list.grid(column=0, row=0, sticky='nsew')
 
+        self.titles_list = titles_list
+
         frame.columnconfigure(0, weight=1)
         frame.columnconfigure(1, weight=0)
         frame.rowconfigure(0, weight=1)
+
+    def item_selected(self, _):
+        iid = self.get_selected_item_iid()
+        if iid is not None:
+            self.iid_last_selected_item = iid
+        self.item_selected_callback(iid)
+
+    def get_selected_item_iid(self) -> int | None:
+        selected_items = self.titles_list.selection()
+        if len(selected_items) == 0:
+            return None
+        else:
+            return int(selected_items[0])
+
+    def update_visible_list(self):
+        content = self.get_content_callback()
+
+        self.titles_list.delete(*self.titles_list.get_children())
+
+        existing_ids = content.existing_ids()
+        existing_ids.sort()
+
+        for i in existing_ids:
+            item = content.get_item_by_id(i)
+            self.titles_list.insert(parent='', index='end', iid=f'{i}',
+                                           text=f'iid{i}',
+                                           values=(item.title, ))
--- user_interface/components/main_frame.py
+++ user_interface/components/main_frame.py
@@ -1,19 +1,27 @@
-from tkinter import Tk, ttk
+from tkinter import Tk, ttk, simpledialog, messagebox
+from typing import Callable
+import os
+
+from tulha import ItemsCompilation
 
 from .top_menu import TopMenu
 from .content_screen import ContentScreen
 
 
-class MainFrame:
+class MainFrame():
     def __init__(self, root_window: Tk,
                  save_file_callback: None,
-                 open_file_callback: None):
+                 open_file_callback:
+                 Callable[[str, str],
+                          tuple[ItemsCompilation | None, str | None]]):
+        self.open_file_callback = open_file_callback
+
         frame = ttk.Frame(root_window, padding="2 2 2 2")
         frame.grid(column=0, row=0, sticky="nsew")
 
         self.top_menu = TopMenu(frame,
                                 save_file_callback,
-                                open_file_callback,
+                                self.request_open_file_callback,
                                 None, None)
 
         self.content_screen = ContentScreen(frame)
@@ -21,3 +29,41 @@ class MainFrame:
         frame.columnconfigure(0, weight=1)
         frame.rowconfigure(0, weight=0)
         frame.rowconfigure(1, weight=1)
+
+    def request_open_file_callback(self, filepath: str):
+        try:
+            self.request_password_and_open(filepath)
+
+        except Exception as e:
+            if hasattr(e, 'message'):
+                message = e.message
+            else:
+                message = f'{e}'
+            messagebox.showerror(title='Error opening file',
+                                 message=f'{message}')
+
+    def request_password_and_open(self, filepath):
+        base_file_name = os.path.splitext(os.path.basename(filepath))[0]
+        user_password = simpledialog.askstring(
+            title='Password Required',
+            prompt=f'Enter the password for \"{base_file_name}\"',
+            show='*')
+        if user_password is None:
+            return
+
+        new_compilation, errors = self.open_file_callback(
+            filepath, user_password)
+
+        if new_compilation is None:
+            raise RuntimeError(
+                f'File {base_file_name} does not contain valid content')
+
+        self.load_new_compilation(new_compilation, errors)
+
+    def load_new_compilation(self, new_compilation: ItemsCompilation,
+                             errors: str | None):
+        if errors is not None:
+            messagebox.showwarning(title='Problems opening file',
+                                   message=f'{errors}')
+
+        self.content_screen.load_new_compilation(new_compilation)
