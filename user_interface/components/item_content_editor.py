104adba3b35d838c6ab8887c12317ac83237a464
Mateus Kahler <mateus.kahler@gmail.com>
2022-09-11 19:36:27 -0300

    basic edit content flow

--- user_interface/components/item_content_editor.py
+++ user_interface/components/item_content_editor.py
@@ -7,8 +7,10 @@ from tulha import ItemsCompilation
 
 class ItemContentEditor:
     def __init__(self, parent: Frame,
-                 get_content_callback: Callable[[], ItemsCompilation]):
+                 get_content_callback: Callable[[], ItemsCompilation],
+                 item_edited_callback):
         self.get_content_callback = get_content_callback
+        self.item_edited_callback = item_edited_callback
 
         frame = ttk.Frame(
             parent, padding="5 5 5 5", style="EstiloY.TFrame")
@@ -20,10 +22,7 @@ class ItemContentEditor:
         def enter_on_keypad(_):
             self.editable_field.insert(tk.INSERT, '\n')
 
-        def modification_callback(_):
-            pass
-
-        editable_field.bind('<<TextModified>>', modification_callback)
+        editable_field.bind('<<TextModified>>', self.item_modified)
         editable_field.bind('<KP_Enter>', enter_on_keypad)
 
         self.editable_field = editable_field
@@ -31,15 +30,25 @@ class ItemContentEditor:
         frame.grid(column=1, row=0, sticky="nsew")
 
     def load_item_text(self, item_iid: int | None):
+        # unbind text modification event, so the load does not trigger
+        # unnecessary callbacks (the load modifies the content)
+        self.editable_field.unbind('<<TextModified>>')
+        self.editable_field.config(state='normal')
+
         if item_iid is None:
             self.editable_field.delete('1.0', 'end')
             self.editable_field.config(state='disabled')
         else:
             content = self.get_content_callback().get_item_by_id(item_iid).text
-
-            self.editable_field.config(state='normal')
             self.editable_field.delete('1.0', 'end')
-            self.editable_field.insert('end', content)
+            self.editable_field.insert('1.0', content)
+
+        self.editable_field.edit_reset()
+        self.editable_field.bind('<<TextModified>>', self.item_modified)
+
+    def item_modified(self, _):
+        content = self.editable_field.get("1.0", "end-1c")
+        self.item_edited_callback(content)
 
 
 class ObservableText(tk.Text):
