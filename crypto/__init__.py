975971bb47786c5e3587ec777de123d96cefde74
Mateus Kahler <mateus.kahler@gmail.com>
2022-09-11 19:38:15 -0300

    expose UI startup script

---
+++ crypto/__init__.py
@@ -0,0 +1,3 @@
+from .storage import write_to_file, load_from_file
+
+__all__ = ['write_to_file', 'load_from_file']
--- kriptoTulhaUI.py
+++ kriptoTulhaUI.py
@@ -1,3 +1,5 @@
+from tulha import ItemsCompilation, loads_from_stream
+from crypto import load_from_file, write_to_file
 from user_interface import KryptoTulhaUserInterface
 
 
@@ -5,8 +7,20 @@ def main():
     open_main_window()
 
 
+def load_file(path: str, user_password: str) \
+        -> tuple[ItemsCompilation | None, str | None]:
+    content = load_from_file(path, user_password)
+    return loads_from_stream(content)
+
+
+def save_file(compilation: ItemsCompilation, path: str, user_password: str) \
+        -> None:
+    content = compilation.dumps()
+    write_to_file(path, content, user_password)
+
+
 def open_main_window():
-    app = KryptoTulhaUserInterface([], None, None)
+    app = KryptoTulhaUserInterface(save_file, load_file)
     app.assume_control()
 
 
